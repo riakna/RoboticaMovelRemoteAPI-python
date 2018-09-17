4bd697b37d9a67ff616b83c8f264e2d892a070fc
diogohs <diogohshiraishi@gmail.com>
2018-09-16 22:38:14 -0300

    Tentativa de odometria

--- mapeamento_brick.py
+++ mapeamento_brick.py
@@ -1,17 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep  2 10:04:06 2018
 
-@author: Anderson
-"""
-import vrep
-import sys
+try:
+    import vrep
+except:
+    print ('--------------------------------------------------------------')
+    print ('"vrep.py" could not be imported. This means very probably that')
+    print ('either "vrep.py" or the remoteApi library could not be found.')
+    print ('Make sure both are in the same folder as this file,')
+    print ('or appropriately adjust the file "vrep.py"')
+    print ('--------------------------------------------------------------')
+    print ('')
+
 import time
+import cv2
 import numpy as np
-import math
 import matplotlib.pyplot as plt
-
+import math
 import _thread
+import odometry
 
 def input_thread(a_list):
     input()
@@ -19,166 +24,211 @@ def input_thread(a_list):
 
 PI = math.pi
 
-# Conexao com V-REP
-vrep.simxFinish(-1)
-clientID = vrep.simxStart('127.0.0.1', 25100, True, True, 2000, 5)
+def trig_circle(angle):
+    if angle < 0:
+        return 2 * PI + angle
+    else:
+        return angle
+
+print("Simulação iniciada!!")
+vrep.simxFinish(-1) #fecha todas as conexões existentes
+
+port = 25100
+
+clientID = vrep.simxStart('127.0.0.1', port, True, True, 5000, 5)
 
 if clientID != -1:
-    print('Connected to remote API server')
+    print("Conectado ao servidor remote API na porta ", port)
 
-else:
-    print('Connection not successful')
-    sys.exit('Could not connect')
+    errCode, left_motor_handle = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor',vrep.simx_opmode_oneshot_wait)
 
-# Handle do robo
-errorCode, robot_handle = vrep.simxGetObjectHandle(clientID,
-                                                   'Pioneer_p3dx', vrep.simx_opmode_oneshot_wait)
+    if errCode != vrep.simx_return_ok:
+        print("Erro em obter o handle")
+    
+    errCode, right_motor_handle = vrep.simxGetObjectHandle(clientID, 'Pioneer_p3dx_rightMotor', vrep.simx_opmode_oneshot_wait)
 
-# Pega os handles dos motores
-errorCode, left_motor_handle = vrep.simxGetObjectHandle(clientID,
-                                                        'Pioneer_p3dx_leftMotor', vrep.simx_opmode_oneshot_wait)
-errorCode, right_motor_handle = vrep.simxGetObjectHandle(clientID,
-                                                         'Pioneer_p3dx_rightMotor', vrep.simx_opmode_oneshot_wait)
+    if errCode != vrep.simx_return_ok:
+        print("Erro em obter o handle")
 
-sensor_handle_list = []
-generated_x_map = []
-generated_y_map = []
+    errCode, robot_handle = vrep.simxGetObjectHandle(clientID, 'Pioneer_p3dx', vrep.simx_opmode_oneshot_wait)
 
-# Posicionamento angular dos sensores
-sensor_angle = np.array([90, 50, 30, 10, -10, -30, -50, -90])
-sensor_angle = sensor_angle * PI / 180.0
+    if errCode != vrep.simx_return_ok:
+        print("Erro em obter o handle")
 
-# Posicionamento do robo
-returnCode, position = vrep.simxGetObjectPosition(
-    clientID, robot_handle, -1, vrep.simx_opmode_streaming)
+    errCode, cam = vrep.simxGetObjectHandle(clientID, 'camera', vrep.simx_opmode_oneshot_wait)
+    if errCode != vrep.simx_return_ok:
+        print("Erro em obter o handle")
 
-returnCode, eulerAngles = vrep.simxGetObjectOrientation(
-    clientID, robot_handle, -1, vrep.simx_opmode_streaming)
 
-# Primeira leitura dos handles
-for x in range(0, 8):
-    errorCode, sensor_handle = vrep.simxGetObjectHandle(clientID,
-                                                        'Pioneer_p3dx_ultrasonicSensor' + str(x + 1), vrep.simx_opmode_oneshot_wait)
-    sensor_handle_list.append(sensor_handle)
-    errorCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(
-        clientID, sensor_handle, vrep.simx_opmode_streaming)
+    # # Armazena o posicionamento angular dos sensores
+    # sensor_angle = np.array([PI/2, 50/180.0*PI, 30/180.0*PI, 10/180.0*PI, 
+    #                    -10/180.0*PI, -30/180.0*PI, -50/180.0*PI, -PI/2, -PI/2, 
+    #                    -130/180.0*PI, -150/180.0*PI, -170/180.0*PI, 170/180.0*PI,
+    #                    150/180.0*PI, 130/180.0*PI, PI/2])
+    # Posicionamento angular dos sensores
+    sensor_angle = np.array([90, 50, 30, 10, -10, -30, -50, -90, -90, -130, -150, -170, 170, 150, 130, 90])
+    sensor_angle = sensor_angle * PI / 180.0
+
+    # recupera a posição do robô
     returnCode, position = vrep.simxGetObjectPosition(
-        clientID, sensor_handle, robot_handle, vrep.simx_opmode_streaming)
+        clientID, robot_handle, -1, vrep.simx_opmode_streaming)
+
+    returnCode, eulerAngles = vrep.simxGetObjectOrientation(
+        clientID, robot_handle, -1, vrep.simx_opmode_streaming)
+
+    startTime = time.time()
+
+    sensor_handle_list = []
+
+    for i in range(16):
+        errCode, sensor_handle = vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_ultrasonicSensor'+str(i+1), vrep.simx_opmode_oneshot_wait)
+        sensor_handle_list.append(sensor_handle)
+        errCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID, sensor_handle, vrep.simx_opmode_streaming) 
+        errCode, position = vrep.simxGetObjectPosition(clientID, sensor_handle, robot_handle, vrep.simx_opmode_streaming)
 
 
-# Pega o posicionamento relativo de cada sonar ao robo
-sensor_position = []
-for x in range(0, 8):
-    while (True):
-        returnCode, position = vrep.simxGetObjectPosition(
-            clientID, sensor_handle_list[x], robot_handle, vrep.simx_opmode_buffer)
-        if (returnCode == 0):
-            break
-    sensor_position.append((position[0], position[1]))
+    errCode, signalValue = vrep.simxGetStringSignal(clientID, "measuredDataAtThisTime",vrep.simx_opmode_streaming)
+    measuredData=vrep.simxUnpackFloats(signalValue)
+
+    # print(measuredData)
+
+    def setVelocity(v_left, v_right):
+        vrep.simxSetJointTargetVelocity(clientID, left_motor_handle, v_left, vrep.simx_opmode_streaming)
+        vrep.simxSetJointTargetVelocity(clientID, right_motor_handle, v_right, vrep.simx_opmode_streaming)
+
+    sensor_position_to_robot = []
+    # Recupera a posição relativa de cada sensor para a posição do robô
+    for i in range(16):
+        while True:
+            errCode, position = vrep.simxGetObjectPosition(clientID, sensor_handle_list[i], robot_handle, vrep.simx_opmode_buffer)
+            if errCode == 0:
+                break
+        sensor_position_to_robot.append((position[0], position[1]))
+
+    map_x = []
+    map_y = []
+
+    gt_x = []
+    gt_y = []
+
+    odom_x = []
+    odom_y = []
+
+    x_laser = []
+    y_laser = []
+
+    robot_path_odom_x = []
+    robot_path_odom_y = []
+
+    a_list = []
+    _thread.start_new_thread(input_thread, (a_list,))
+
+    odometry_thread = odometry.odometry()
+    odometry.start_thread(clientID, robot_handle, left_motor_handle, right_motor_handle)
+
+    #number_of_turns = 0
+    while not a_list and vrep.simxGetConnectionId(clientID)!=-1:
+        sensor_dist_values = []
+        sensor_points_to_robot = []
+
+        errCode, signalValue = vrep.simxGetStringSignal(clientID, "measuredDataAtThisTime",vrep.simx_opmode_buffer)
+        measuredData=vrep.simxUnpackFloats(signalValue)
+
+        laser_array = np.reshape(measuredData, (int(len(measuredData)/3),3))
+
+        errCode, position = vrep.simxGetObjectPosition(clientID, robot_handle, -1, vrep.simx_opmode_buffer)
+        errCode, eulerAngles = vrep.simxGetObjectOrientation(clientID, robot_handle, -1, vrep.simx_opmode_buffer)
+
+        gt_x.append(position[0])
+        gt_y.append(position[1])
+
+        robot_angle = math.radians(math.degrees(eulerAngles[2])%360.0)
+        # print(math.degrees(robot_angle))
+
+        x_odom, y_odom = odometry_thread.getPos()
+        robot_path_odom_x.append(x_odom)
+        robot_path_odom_y.append(y_odom)
+
+        # print(x_odom, ' ', y_odom)
+
+        x_laser = np.append(x_laser, laser_array[:,0]*np.cos(robot_angle)-laser_array[:,1]*np.sin(robot_angle)+position[0])
+        y_laser = np.append(y_laser, laser_array[:,0]*np.sin(robot_angle)+laser_array[:,1]*np.cos(robot_angle)+position[1])
 
 
-def setVelocity(left, right):
-    vrep.simxSetJointTargetVelocity(
-        clientID, left_motor_handle, left, vrep.simx_opmode_streaming)
-    vrep.simxSetJointTargetVelocity(
-        clientID, right_motor_handle, right, vrep.simx_opmode_streaming)
+        # plt.scatter(x_laser, y_laser, s=2)
+        plt.scatter(x_laser, y_laser, s=2, c='gray')
+        plt.scatter(gt_x, gt_y, s=2, c='green')
+        plt.scatter(robot_path_odom_x, robot_path_odom_y, s=2, c='red')
+        plt.axis('equal')
+        plt.pause(0.001)
 
-setVelocity(2, 2)
-t = time.time()
-turning_left = False
-turning_right = False
+        for i in range(1,16+1):
+            errCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(clientID, sensor_handle_list[i-1], vrep.simx_opmode_buffer)
 
-a_list = []
-_thread.start_new_thread(input_thread, (a_list,))
+            # gt_x.append(position[0])
+            # gt_y.append(position[1])
 
-while not a_list:
-# while (time.time() - t) < 60:
+            if detectionState == True:
+                distance = np.linalg.norm(detectedPoint)
+                sensor_dist_values.append(distance)
 
-    sensor_val = []
+                if distance < 0.5 and distance >= 0.2:
+                    x_obs_robot = detectedPoint[2]*np.cos(sensor_angle[i-1])-detectedPoint[0]*np.sin(sensor_angle[i-1])+sensor_position_to_robot[i-1][0]
+                    y_obs_robot = detectedPoint[2]*np.sin(sensor_angle[i-1])-detectedPoint[0]*np.cos(sensor_angle[i-1])+sensor_position_to_robot[i-1][1]
+                    sensor_points_to_robot.append((x_obs_robot, y_obs_robot))
 
-    # Faz leitrua dos sonares a cada iteração
-    for x in range(1, 8 + 1):
+                    x_robot_world = x_obs_robot*np.cos(eulerAngles[2])-y_obs_robot*np.sin(eulerAngles[2])+position[0]
+                    y_robot_world = x_obs_robot*np.sin(eulerAngles[2])-y_obs_robot*np.cos(eulerAngles[2])+position[1]
+                    map_x.append(x_robot_world)
+                    map_y.append(y_robot_world)
+            else:
+                sensor_dist_values.append(np.inf)
+                sensor_points_to_robot.append((0,0))
+        
+        # setVelocity(2, 2)
 
-        # Distancia detectada pelo sonar
-        errorCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = vrep.simxReadProximitySensor(
-            clientID, sensor_handle_list[x - 1], vrep.simx_opmode_buffer)
-        distance = np.linalg.norm(detectedPoint)
+        # if sensor_dist_values[4] < 0.4 or sensor_dist_values[5] < 0.4 or sensor_dist_values[3] < 0.4 or sensor_dist_values[2] < 0.4:
+        #     setVelocity(-1, -2)#vira a esquerda
+        #     time.sleep(2)
 
-        # Posicao global do robo
-        returnCode, position = vrep.simxGetObjectPosition(
-            clientID, robot_handle, -1, vrep.simx_opmode_buffer)
-        returnCode, eulerAngles = vrep.simxGetObjectOrientation(
-            clientID, robot_handle, -1, vrep.simx_opmode_buffer)
+        act_dist = 0.35
 
-        if (detectionState == True):
+        cont_left = 0
+        cont_right = 0
 
-            sensor_val.append(distance)
+        for i in range(1, 7):
+            if i < 4:
+                if sensor_dist_values[i] < act_dist:
+                    cont_left += 1
+            else:
+                if sensor_dist_values[i] < act_dist:
+                    cont_right += 1
 
-            # Coordadena do obstaculo é dado pela posicão global do
-            # sonar (global robo + relativo sonar) + a projeção
-            # da distancia detectada nos eixos
-            x_obstacle = position[0] + (sensor_position[x-1][0] + distance) * (np.cos(eulerAngles[2]+sensor_angle[x-1]))
-            y_obstacle = position[1] + (sensor_position[x-1][1] + distance) * (np.sin(eulerAngles[2]+sensor_angle[x-1]))
-            # y_obstacle = detectedPoint[1] + position[1]
-            # x_obstacle = detectedPoint[0] + position[0]
-
-            generated_x_map.append(x_obstacle)
-            generated_y_map.append(y_obstacle)
-
+        if sensor_dist_values[2] > act_dist and sensor_dist_values[3] > act_dist and sensor_dist_values[4] > act_dist and sensor_dist_values[5] > act_dist:
+            setVelocity(3, 3)
+        elif cont_left > cont_right:
+            setVelocity(2, -2) 
         else:
-            sensor_val.append(np.inf)
+            setVelocity(-2, 2)
 
-    # Pega o menor valor dos sensores
-    # min_ind = np.where(np.array(sensor_val) == np.min(sensor_val))
-    # min_ind = min_ind[0][0]
+        # time.sleep(0.2)
 
-    # se o menor valor é dos sensores frontais
-    # if (sensor_val[min_ind] < 0.5) and (0 < min_ind < 7):
-    #     if (0 < min_ind <= 3) and not turning_right:
-    #         setVelocity(1, -1)
-    #         turning_left = True
-    #     elif (4 <= min_ind < 7) and not turning_left:
-    #         setVelocity(-1, 1)
-    #         turning_right = True
-    # else:
-    #     setVelocity(2, 2)
-    #     turning_left = False
-    #     turning_right = False
+    print("End of simulation")
 
-    act_dist = 0.25
+    plt.scatter(x_laser, y_laser, s=2, c='gray')
+    plt.scatter(gt_x, gt_y, s=2, c='green')
+    plt.scatter(robot_path_odom_x, robot_path_odom_y, s=2, c='red')
+    # plt.scatter(x_laser, y_laser, s=2, c='gray')
+    plt.axis('equal')
+    plt.pause(0.05)
 
-    cont_left = 0
-    cont_right = 0
+    plt.show()
+    setVelocity(0, 0)
+    vrep.simxGetPingTime(clientID)
 
-    for i in range(1, 7):
-        if i < 4:
-            if sensor_val[i] < act_dist:
-                cont_left += 1
-        else:
-            if sensor_val[i] < act_dist:
-                cont_right += 1
+    # Now close the connection to V-REP:
+    vrep.simxFinish(clientID)
 
-    # if sensor_val[3] < act_dist:
-    #     setVelocity(1, -1)
-    # elif sensor_val[4] < act_dist:
-    #     setVelocity(-1, 1)
-    # elif sensor_val[2] < act_dist:
-    #     setVelocity(1, -1)
-    # elif sensor_val[5] < act_dist:
-    #     setVelocity(-1, 1)
-    if sensor_val[2] > act_dist and sensor_val[3] > act_dist and sensor_val[4] > act_dist and sensor_val[5] > act_dist:
-        setVelocity(3, 3)
-    elif cont_left > cont_right:
-        setVelocity(2, -2) 
-    else:
-        setVelocity(-2, 2)
-
-    # plt.scatter(generated_x_map, generated_y_map, s=2)
-    # plt.show()
-    # Considere execucao do loop a cada 0.2 segundos (= 5 Hz)
-    time.sleep(0.2)
-
-plt.scatter(generated_x_map, generated_y_map, s=2)
-plt.show()
-
-setVelocity(0, 0)
+    cv2.destroyAllWindows()
+else:
+    print("Falha na conexão com o servidor remote API")
